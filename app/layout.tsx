f8c58b5fe9ff17334422113e0774d0af5d8cdc41
FAJAR MUKEMEN <43581831+mukemen@users.noreply.github.com>
2025-09-03 00:51:21 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-// app/layout.tsx
 import "./globals.css";
 
 export const metadata = {
@@ -10,18 +9,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id">
       <head>
-        {/* PWA & Icon */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon-192.png" />
         <meta name="theme-color" content="#0b0b0b" />
-
-        {/* Mobile meta */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-
-        {/* iOS PWA fullscreen */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
